ce0b81bc4c90b3d4f5a19e7cdeb888b4e91c3a68
jihoon_gweon <kjh8673a@naver.com>
2023-06-05 15:23:20 +0900

    [Feat] 약 검색 필터

--- front/src/pages/Drug.js
+++ front/src/pages/Drug.js
@@ -1,12 +1,18 @@
 import Header from 'components/common/Header'
+import MainSearchBar from 'components/common/MainSearchBar'
+import DrugFilter from 'components/drug/DrugFilter'
 import React from 'react'
 import { connect } from 'react-redux'
 
 export const Drug = (props) => {
   return (
-    <div className="bg-opacity-10 bg-[#00C192] w-screen h-screen overflow-hidden">
+    <div className="bg-opacity-10 bg-[#00C192] w-screen h-screen overflow-hidden mx-auto">
       <Header />
-      <div>약 검색</div>
+      <div>
+        <MainSearchBar color={"bg-[#D7F1FF]"} />
+        <DrugFilter />
+        <div className='absolute left-[16px] top-[760px] text-center leading-[80px] w-[380px] h-[80px] bg-[#00C192] rounded-[10px] text-3xl text-white font-bold placeholder-[#A1AFA9]'>검색</div>
+      </div>
     </div>
   )
 }
